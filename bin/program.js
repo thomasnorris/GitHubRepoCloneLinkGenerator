12619f8273fb5a64884e6d5597be60f37810f996
Thomas Norris <tom.norris135@gmail.com>
2019-02-01 12:18:13 -0500

    added constants and started to build the request

--- bin/program.js
+++ bin/program.js
@@ -1,3 +1,21 @@
 (function() {
+    const GITHUB_API_BASE_URL = 'https://api.github.com';
+    const GITHUB_REPOS_URL = '/user/repos';
+    const AUTH_FILE_NAME = 'OAuthToken.txt';
+
+    var _request = require('request');
+
+    var requestOptions = {
+        url: '\"' + GITHUB_API_BASE_URL + GITHUB_REPOS_URL + '\"',
+        headers: {
+            'Authorization': 'Bearer ' + GetOAuthToken()
+        }
+    };
+
+    console.log(requestOptions);
+
+    function GetOAuthToken() {
+        return 'fsdfsdfdsdf';
+    }
 
 })();

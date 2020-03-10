3b207f64112dc6fa0aeb49521107b857f932280f
Thomas Norris <tom.norris135@gmail.com>
2020-03-10 11:14:18 -0400

    added readline-sync; options for adding clone alias and auto cd into folder

--- bin/program.js
+++ bin/program.js
@@ -2,12 +2,13 @@
     // The name of the SSH config file Host to use (optional parameter)
     var _ssh_config_name = process.argv.slice(2)[0];
 
-    var _rl = require('readline');
+    var _rl = require('readline-sync');
     var _clip = require('clipboardy');
 
     const GITHUB_API_BASE_URL = 'https://api.github.com';
     const GITHUB_REPOS_URL = '/user/repos';
     const AUTH_FILE_FULL_PATH = __dirname + '\\' + 'OAuthToken.txt';
+    const CLONE_ALIAS = 'g clo';
 
     GetOAuthToken((token) => {
         var request = require('request');
@@ -20,42 +21,47 @@
         };
 
         request(requestOptions, (err, res, body) => {
-            var rl = _rl.createInterface({
-                input: process.stdin,
-                output: process.stdout
-            });
-
             if (!err && res.statusCode === 200) {
                 var repos = [];
                 var count = 0;
                 JSON.parse(body).forEach((key) => {
                     var url = key.ssh_url;
                     var full_name = key.full_name;
+                    var folder = key.name;
 
                     if (_ssh_config_name)
                         url = url.replace('github.com', _ssh_config_name);
 
                     repos.push({
                         url: url,
-                        full_name: full_name
+                        full_name: full_name,
+                        folder: folder
                     });
 
                     console.log(count++ + ': ' + full_name);
                 });
 
-                rl.question('\nEnter repo index to copy [0 - ' + (repos.length - 1) +'] [-1 to exit]: ', (index) => {
-                    rl.close();
-                    if (index === '-1')
-                        Exit('Exiting.');
+                var index = _rl.question('\nChoose repo [0 - ' + (repos.length - 1) +'][-1 to exit]: ');
+                if (index === '-1')
+                    Exit('Exiting.');
 
-                    var repo = repos[index];
-                    if (!repo)
-                        Exit('Undefined index.');
+                var repo = repos[index];
+                if (!repo)
+                    Exit('Undefined index.');
 
-                    _clip.writeSync(repo.url);
+                var link = repo.url;
+                var addAlias = _rl.question('Add "' + CLONE_ALIAS + '" to the beginning? [y/n][default is y]: ');
+                if (addAlias.toLowerCase() !== 'n')
+                    link = CLONE_ALIAS + ' ' + link;
 
-                    Exit('Clone link for \"' + repo.full_name + '\" copied to the clipboard.');
-                });
+                var cdRepo = ' && cd ' + repo.folder;
+                var copyFolder = _rl.question('Add "' + cdRepo + '" at the end? [y/n][default is y]: ');
+                if (copyFolder.toLowerCase() !== 'n')
+                    link += cdRepo;
+
+                _clip.writeSync(link);
+
+                Exit('\nLink for repo "' + repo.full_name + '" copied to clipboard.');
             }
             else
                 Exit('There was an error with the request. Status code: ' + res.statusCode)
